501279c19b446f895a2332f814f6d743045f2e5c
Surya Prakash <surya512002@gmail.com>
2026-01-12 10:51:11 +0530

    greetusers.go

--- greetusers.go
+++ greetusers.go
@@ -6,4 +6,5 @@ func greetUsers() {
 	fmt.Printf("welcome to my %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets to attend")
+	fmt.Println("---------------------------------------------------")
 }
